4dbf57f73fcb26193814d52bfd835f500a42a398
= <kevin.burdin@etu.univ-lyon1.fr>
2021-12-14 16:43:20 +0100

    data extraction

--- testsAPISteam/dataExtract.py
+++ testsAPISteam/dataExtract.py
@@ -7,9 +7,12 @@ import pandas as pd
 
 
 url = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/"
+url2 = "http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/"
+url3 = "http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/"
 key = "F6F2A22B759FEE0F79940A8783603562"
-dictionary = {"key" : "F6F2A22B759FEE0F79940A8783603562", "steamId":"76561198119517741"}
-req = webapi_request(url,params=dictionary)
-df = pd.DataFrame(req["response"]["games"])
-df.to_csv("~/games.csv")
+dictionary = {"key" : "F6F2A22B759FEE0F79940A8783603562", "steamId":"76561198119517741", "appId" : "567640"}
+req = webapi_request(url3,params=dictionary)
+print(req)
+df = pd.DataFrame(req["playerstats"])
+df.to_csv("~/danganronpa.csv")
 
