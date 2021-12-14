4d4876b50911e94382b1a296e03b35d3dac2a9a9
GG <gael.garcia@etu.univ-lyon1.fr>
2021-12-14 16:29:30 +0100

    fais pas a dans le rep de connexion

--- testsAPISteam/dataExtract.py
+++ testsAPISteam/dataExtract.py
@@ -8,8 +8,8 @@ import pandas as pd
 
 url = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/"
 key = "F6F2A22B759FEE0F79940A8783603562"
-dictionary = {"key" : "F6F2A22B759FEE0F79940A8783603562", "steamId":"76561198119517741"}
+dictionary = {"key" : "F6F2A22B759FEE0F79940A8783603562", "steamId":"76561198096382864"}
 req = webapi_request(url,params=dictionary)
 df = pd.DataFrame(req["response"]["games"])
-df.to_csv("~/games.csv")
+df.to_csv("./games.csv")
 
