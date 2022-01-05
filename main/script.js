7d5f354f1543ebc47ff635ef9fdf5eb270a380aa
GG <gael.garcia@etu.univ-lyon1.fr>
2022-01-05 17:50:34 +0100

    On etoffe

--- main/script.js
+++ main/script.js
@@ -1,3 +1,15 @@
 function display() {  
     alert("Hammer time");  
-    }  
+    } 
+
+const margin = { top: 0, right: 30, bottom: 20, left: 10 },
+    width = 960,
+    height = 960;
+    
+var svg1 = d3
+.select("#visu-activite1")
+.append("svg")
+.attr("width", width)
+.attr("height", height)
+.append("g")
+.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
