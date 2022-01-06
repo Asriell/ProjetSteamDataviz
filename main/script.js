7d632ed3b64f49a7236cc32627957fb920ae50e6
GG <gael.garcia@etu.univ-lyon1.fr>
2022-01-06 09:28:20 +0100

    get csv

--- main/script.js
+++ main/script.js
@@ -5,11 +5,20 @@ function display() {
 const margin = { top: 0, right: 30, bottom: 20, left: 10 },
     width = 960,
     height = 960;
+
+function display_graph1() {
+    var svg1 = d3
+    .select("#visu-activite1")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    console.log("Shit")
     
-var svg1 = d3
-.select("#visu-activite1")
-.append("svg")
-.attr("width", width)
-.attr("height", height)
-.append("g")
-.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    urlRaw = "https://raw.githubusercontent.com/Asriell/ProjetSteamDataviz/main/main/games.csv"
+    d3.csv(urlRaw, (d) => {
+        console.log(d)
+    })
+}
