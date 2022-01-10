d934e79c87866d84633384b121620b6671582c97
Kevin BURDIN <k.burdin@hotmail.fr>
2022-01-10 20:59:38 +0100

    rearrangement de la legende

--- merge/bar_chart1.js
+++ merge/bar_chart1.js
@@ -477,7 +477,7 @@ function addLegend(colors,keys,total_width,start_margin,margin, legendPerLines =
                     .select("svg1")
                     .append("svg")
                     .attr("width", total_width)
-                    .attr("height", keys.length)
+                    .attr("height", 30 * keys.length)
                     .attr(
                         "transform",
                         "translate(" + start_margin + "," + margin + ")"
