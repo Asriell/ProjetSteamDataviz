242ff1dbd4d3547893a5d2877b8fb4115327689b
= <kevin.burdin@etu.univ-lyon1.fr>
2022-01-11 11:17:42 +0100

    tags

--- merge/pie_chart3.js
+++ merge/pie_chart3.js
@@ -101,7 +101,7 @@ function display_graph3(svg_already_exists,svg3) {
                 console.log(Object.keys(gameDescriptions),"   ",Object.keys(gameDescriptions).length)
                 tags = {}
                 for(id of Object.keys(gamesIds)) {
-                    tags[id] = gameDescriptions.genres; 
+                    tags[id] = gameDescriptions[id].genres; 
                 }
                 console.log(tags);
             });
