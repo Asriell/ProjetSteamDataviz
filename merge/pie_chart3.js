723eadca9175d060bd45856548f22ee0b886d6bc
Kevin BURDIN <k.burdin@hotmail.fr>
2022-01-11 21:00:07 +0100

    extraction description jeux

--- merge/pie_chart3.js
+++ merge/pie_chart3.js
@@ -144,11 +144,83 @@ function display_graph3(svg_already_exists,svg3) {
                 }
                 delete genreTimePerPeriod["Early Access"];
                 delete genreTimePerPeriod["Free to Play"];
+                id = 0;
                 for(genre of Object.keys(genreTimePerPeriod)) {
                     timeArray = genreTimePerPeriod[genre].split(":");
                     genreTimePerPeriod[genre] = timeArray[0]*3600 + timeArray[1] * 60 + timeArray[0];
                 }
                 console.log(genreTimePerPeriod);
+
+                var color = d3.scaleOrdinal()
+                            .domain([0, d3.max(datas, function (d) { return d; })])
+                            .range(['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
+                                '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+                                '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A',
+                                '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+                                '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC',
+                                '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+                                '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680',
+                                '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+                                '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3',
+                                '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'])
+                var pie = d3.pie()
+                .value(function (d) {
+                    console.log("pie value : " + d);
+                    return d.total_playtime;
+                })
+        
+                var data_ready = pie(genreTimePerPeriod)
+
+                svg3
+                    .selectAll('arcs')
+                    .data(data_ready)
+                    .enter()
+                    .append('path')
+                    .attr('d', arcGenerator)
+                    .attr('fill', function (d) { console.log("pie generation : ", d ); return (d); })
+                    .attr("class","pie")
+                    .on("mousemove", function (e, d) {
+                        // on recupere la position de la souris,
+                        // e est l'object event d
+                        theData = d.data;
+                        var mousePosition = [e.x, e.y];
+                        //console.log(mousePosition);
+                        // on affiche le toolip
+                        tooltip
+                            .classed("hidden", false)
+                            // on positionne le tooltip en fonction
+                            // de la position de la souris
+                            .attr(
+                                "style",
+                                "left:" +
+                                (mousePosition[0] + 15) +
+                                "px; top:" +
+                                (mousePosition[1] - 35) +
+                                "px"
+                            )
+                            // on recupere le nom de l'etat
+                            .html(
+                                theData.date +
+                                " | Temps de jeu : " +
+                                parseInt(theData.total_playtime / 3600) +
+                                " h " +
+                                parseInt(
+                                    (theData.total_playtime - parseInt(theData.total_playtime / 3600) * 3600) / 60
+                                ) +
+                                " m " +
+                                (theData.total_playtime -
+                                    (parseInt(theData.total_playtime / 3600) * 3600 +
+                                        parseInt(
+                                            (theData.total_playtime - parseInt(theData.total_playtime / 3600) * 3600) / 60
+                                        ) *
+                                        60)) +
+                                " s."
+                            );
+                    })
+                    .on("mouseout", function () {
+                        tooltip.classed("hidden", true);
+                    });
+                
             });
     });
 
