13fe5f49c0272aae420bc3b952522d793ac0ff3b
Kevin BURDIN <k.burdin@hotmail.fr>
2022-01-12 03:02:12 +0100

    correction

--- template/assets/js/pie_chart3.js
+++ template/assets/js/pie_chart3.js
@@ -145,9 +145,10 @@ function display_graph3(svg_already_exists,svg3) {
         id = 0;
         for(genre of Object.keys(genreTimePerPeriod)) {
             timeArray = genreTimePerPeriod[genre].split(":");
-            genreTimePerPeriod[genre] = timeArray[0]*3600 + timeArray[1] * 60 + timeArray[0];
+            console.log("TA : " , timeArray)
+            genreTimePerPeriod[genre] = timeArray[0]*3600 + timeArray[1] * 60 + timeArray[2];
         }
-        console.log(genreTimePerPeriod);
+        console.log("GTPP : " , genreTimePerPeriod);
         
         datas = [];
         id = 0;
